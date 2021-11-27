32c1112bc0e8f57b7b81d47413e0f1f578e4ef1e
whdydrl <gi4004194@gmail.com>
2021-11-28 06:14:59 +0900

    -feat

---
+++ 1330.py
@@ -0,0 +1,8 @@
+a,b = map(int, input())
+
+if a > b:
+    print(">")
+elif a < b:
+    print("<")
+elif a == b:
+    print("==")
---
+++ 9498.py
@@ -0,0 +1,12 @@
+reslut = int(input())
+
+if reslut >= 90:
+    print("A")
+elif reslut >= 80:
+    print("B")
+elif reslut >= 70:
+    print("C")
+elif reslut >= 60:
+    print("D")
+else:
+    print("F")
